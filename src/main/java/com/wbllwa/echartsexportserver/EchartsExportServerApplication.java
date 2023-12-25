3f56a6b2f8f4d0366bf3e265613d99e9e2694aaf
libw <libowen.dev@foxmail.com>
2023-12-25 16:06:26 +0800

    init: 初始化项目

---
+++ src/main/java/com/wbllwa/echartsexportserver/EchartsExportServerApplication.java
@@ -0,0 +1,16 @@
+package com.wbllwa.echartsexportserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author libw
+ */
+@SpringBootApplication
+public class EchartsExportServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchartsExportServerApplication.class, args);
+    }
+
+}
